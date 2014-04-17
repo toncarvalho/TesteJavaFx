dd0d75f9b493a851df9e1ae12c38faff8661f19d
Ton Carvalho <wellington.carvalho@atualcs.com.br>
2014-04-17 15:36:15 -0300

    adição do cpf

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,6 +12,10 @@ public class Controller implements Serializable{
 
     private String email;
 
+    private String cpf;
+
+
+
 
     public String getNome() {
         return nome;
@@ -44,5 +48,14 @@ public class Controller implements Serializable{
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
   }
 }
